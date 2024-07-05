42aa8b5fd695f5d60bff9bcfbc6da4b554dfe16f
Pedro Miguel <pedro.042013@gmail.com>
2024-07-05 07:48:14 -0300

    feat: add base PO UI template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'empodera-frontend';
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
 }
